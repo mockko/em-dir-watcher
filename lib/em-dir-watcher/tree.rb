ed00b9ed0f67f9cf636808c63a3f88c24a13ffb0
Andrey Tarantsov <andreyvit@gmail.com>
2010-08-04 18:26:53 +0700

    Support refreshing a Tree

--- lib/em-dir-watcher/tree.rb
+++ lib/em-dir-watcher/tree.rb
@@ -1,4 +1,6 @@
 
+require 'set'
+
 module EMDirWatcher
 
 class Entry
@@ -8,25 +10,58 @@ class Entry
   def initialize tree, relative_path
     @tree = tree
     @relative_path = relative_path
+    @is_file = false
+    @file_mtime = Time.at(0)
+    @entries = {}
   end
 
   def full_path
     if @relative_path.empty? then @tree.full_path else File.join(@tree.full_path, @relative_path) end
   end
 
-  def entries
-    (Dir.entries(full_path) - ['.', '..']).collect do |entry|
-      entry_relative_path = if @relative_path.empty? then entry else File.join(@relative_path, entry) end
-      next nil if @tree.excludes? entry_relative_path
-      Entry.new @tree, entry_relative_path
-    end.compact
+  def compute_is_file
+    FileTest.file? full_path
+  end
+
+  def compute_file_mtime
+    if FileTest.symlink?(full_path) then Time.at(0) else File.mtime(full_path) end
+  rescue Errno::ENOENT, Errno::ENOTDIR
+    Time.at(0)
+  end
+  
+  def compute_entry_names
+    Dir.entries(full_path) - ['.', '..']
+  rescue Errno::ENOENT, Errno::ENOTDIR
+    []
+  end
+
+  def compute_entries
+    new_entries = {}
+    compute_entry_names.each do |entry_name|
+      entry_relative_path = if @relative_path.empty? then entry_name else File.join(@relative_path, entry_name) end
+      next if @tree.excludes? entry_relative_path
+      new_entries[entry_name] = @entries[entry_name] || Entry.new(@tree, entry_relative_path)
+    end
+    new_entries
+  end
+
+  def refresh! changed_relative_paths
+    used_to_be_file, @is_file = @is_file, compute_is_file
+    previous_file_mtime, @file_mtime = @file_mtime, compute_file_mtime
+    if used_to_be_file
+      changed_relative_paths << @relative_path if not @is_file or previous_file_mtime != @file_mtime
+    elsif @is_file
+      changed_relative_paths << @relative_path
+    end
+    old_entries, @entries = @entries, compute_entries
+    (Set.new(@entries.values) + Set.new(old_entries.values)).each { |entry| entry.refresh! changed_relative_paths }
   end
 
   def recursive_file_entries
-    if FileTest.directory? full_path
-      entries.collect { |entry| entry.recursive_file_entries }.flatten
-    else
+    if @is_file
       self
+    else
+      @entries.values.collect { |entry| entry.recursive_file_entries }.flatten
     end
   end
 
@@ -66,10 +101,11 @@ class Tree
   attr_reader :full_path
   attr_reader :excludes
 
-  def initialize full_path
+  def initialize full_path, exclusions=[]
     @full_path = full_path
+    self.excludes = exclusions
     @root_entry = Entry.new self, ''
-    self.excludes = []
+    @root_entry.refresh! []
   end
   
   def excludes= new_excludes
@@ -83,6 +119,12 @@ class Tree
         NameGlobMatcher.new exclusion
       end
     end
+  end
+  
+  def refresh!
+    changed_relative_paths = []
+    @root_entry.refresh! changed_relative_paths
+    changed_relative_paths.sort
   end
 
   def excludes? relative_path
--- test/test_tree.rb
+++ test/test_tree.rb
@@ -9,21 +9,25 @@ class TestTreeFileList < Test::Unit::TestCase
   def setup
     FileUtils.rm_rf TEST_DIR
     FileUtils.mkdir_p TEST_DIR
-    @tree = EMDirWatcher::Tree.new TEST_DIR
   end
 
   should "should be empty for an empty directory" do
+    @tree = EMDirWatcher::Tree.new TEST_DIR
     assert_equal "", @tree.full_file_list.join(", ").strip
   end
 
   should "should return a single file" do
     FileUtils.touch File.join(TEST_DIR, 'foo')
+
+    @tree = EMDirWatcher::Tree.new TEST_DIR
     assert_equal "foo", @tree.full_file_list.join(", ").strip
   end
 
   should "should return a file in a subdirectory" do
     FileUtils.mkdir File.join(TEST_DIR, 'bar')
     FileUtils.touch File.join(TEST_DIR, 'bar', 'foo')
+
+    @tree = EMDirWatcher::Tree.new TEST_DIR
     assert_equal "bar/foo", @tree.full_file_list.join(", ").strip
   end
 
@@ -32,6 +36,8 @@ class TestTreeFileList < Test::Unit::TestCase
     FileUtils.touch File.join(TEST_DIR, 'zz')
     FileUtils.mkdir File.join(TEST_DIR, 'bar')
     FileUtils.touch File.join(TEST_DIR, 'bar', 'foo')
+
+    @tree = EMDirWatcher::Tree.new TEST_DIR
     assert_equal "aa, bar/foo, zz", @tree.full_file_list.join(", ").strip
   end
 
@@ -55,8 +61,6 @@ class TestTreeExcludes < Test::Unit::TestCase
     FileUtils.touch File.join(TEST_DIR, 'bar', 'boo', 'bizzz')
 
     @list = ['aa', 'biz', 'zz', 'bar/foo', 'bar/biz', 'bar/biz.html', 'bar/boo/bizzz'].sort
-
-    @tree = EMDirWatcher::Tree.new TEST_DIR
   end
 
   def join list
@@ -64,28 +68,124 @@ class TestTreeExcludes < Test::Unit::TestCase
   end
 
   should "ignore a single file excluded by path" do
-    @tree.excludes = ['bar/biz']
+    @tree = EMDirWatcher::Tree.new TEST_DIR, ['bar/biz']
     assert_equal join(@list - ['bar/biz']), join(@tree.full_file_list)
   end
 
   should "ignore files excluded by name" do
-    @tree.excludes = ['biz']
+    @tree = EMDirWatcher::Tree.new TEST_DIR, ['biz']
     assert_equal join(@list - ['biz', 'bar/biz']), join(@tree.full_file_list)
   end
 
   should "ignore files excluded by name glob" do
-    @tree.excludes = ['biz*']
+    @tree = EMDirWatcher::Tree.new TEST_DIR, ['biz*']
     assert_equal join(@list - ['biz', 'bar/biz', 'bar/biz.html', 'bar/boo/bizzz']), join(@tree.full_file_list)
   end
 
   should "ignore a directory excluded by name glob" do
-    @tree.excludes = ['bo*']
+    @tree = EMDirWatcher::Tree.new TEST_DIR, ['bo*']
     assert_equal join(@list - ['bar/boo/bizzz']), join(@tree.full_file_list)
   end
 
   should "ignore a files and directories excluded by regexp" do
-    @tree.excludes = [/b/]
+    @tree = EMDirWatcher::Tree.new TEST_DIR, [/b/]
     assert_equal join(['aa', 'zz']), join(@tree.full_file_list)
+  end
+
+end
+
+class TestTreeRefreshing < Test::Unit::TestCase
+
+  def setup
+    FileUtils.rm_rf TEST_DIR
+    FileUtils.mkdir_p TEST_DIR
+
+    FileUtils.mkdir File.join(TEST_DIR, 'bar')
+    FileUtils.mkdir File.join(TEST_DIR, 'bar', 'boo')
+
+    FileUtils.touch File.join(TEST_DIR, 'aa')
+    FileUtils.touch File.join(TEST_DIR, 'biz')
+    FileUtils.touch File.join(TEST_DIR, 'zz')
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'foo')
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'biz')
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'biz.html')
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'boo', 'bizzz')
+
+    @list = ['aa', 'biz', 'zz', 'bar/foo', 'bar/biz', 'bar/biz.html', 'bar/boo/bizzz'].sort
+  end
+
+  def join list
+    list.join(", ").strip
+  end
+
+  should "no changes when nothing has changed" do
+    @tree = EMDirWatcher::Tree.new TEST_DIR
+    changed_paths = @tree.refresh!
+    assert_equal "", join(changed_paths)
+    assert_equal join(@list), join(@tree.full_file_list)
+  end
+
+  should "single file modification" do
+    @tree = EMDirWatcher::Tree.new TEST_DIR
+    sleep 1
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'foo')
+    changed_paths = @tree.refresh!
+    assert_equal join(['bar/foo']), join(changed_paths)
+  end
+
+  should "single file deletion" do
+    @tree = EMDirWatcher::Tree.new TEST_DIR
+    FileUtils.rm File.join(TEST_DIR, 'bar', 'biz')
+    changed_paths = @tree.refresh!
+    assert_equal join(['bar/biz']), join(changed_paths)
+  end
+
+  should "single directory deletion" do
+    @tree = EMDirWatcher::Tree.new TEST_DIR
+    FileUtils.rm_rf File.join(TEST_DIR, 'bar')
+    changed_paths = @tree.refresh!
+    assert_equal join(['bar/foo', 'bar/biz', 'bar/biz.html', 'bar/boo/bizzz'].sort), join(changed_paths)
+  end
+
+  should "single file creation" do
+    @tree = EMDirWatcher::Tree.new TEST_DIR
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'miz')
+    changed_paths = @tree.refresh!
+    assert_equal join(['bar/miz']), join(changed_paths)
+  end
+
+  should "single directory creation" do
+    @tree = EMDirWatcher::Tree.new TEST_DIR
+    FileUtils.mkdir File.join(TEST_DIR, 'bar', 'koo')
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'koo', 'aaa')
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'koo', 'zzz')
+    changed_paths = @tree.refresh!
+    assert_equal join(['bar/koo/aaa', 'bar/koo/zzz'].sort), join(changed_paths)
+  end
+
+  should "not report changes on empty directory creation" do
+    @tree = EMDirWatcher::Tree.new TEST_DIR
+    FileUtils.mkdir File.join(TEST_DIR, 'bar', 'koo')
+    changed_paths = @tree.refresh!
+    assert_equal "", join(changed_paths)
+  end
+
+  should "files turned into a directory" do
+    @tree = EMDirWatcher::Tree.new TEST_DIR
+    FileUtils.rm File.join(TEST_DIR, 'bar', 'foo')
+    FileUtils.mkdir File.join(TEST_DIR, 'bar', 'foo')
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'foo', 'aaa')
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'foo', 'zzz')
+    changed_paths = @tree.refresh!
+    assert_equal join(['bar/foo', 'bar/foo/aaa', 'bar/foo/zzz'].sort), join(changed_paths)
+  end
+
+  should "directory turned into a file" do
+    @tree = EMDirWatcher::Tree.new TEST_DIR
+    FileUtils.rm_rf File.join(TEST_DIR, 'bar', 'boo')
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'boo')
+    changed_paths = @tree.refresh!
+    assert_equal join(['bar/boo/bizzz', 'bar/boo'].sort), join(changed_paths)
   end
 
 end
