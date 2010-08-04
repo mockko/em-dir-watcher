2dd31d28fcac0e4ab81562fa6fde6d8f6eab3e7a
Andrey Tarantsov <andreyvit@gmail.com>
2010-08-04 17:36:50 +0700

    Excludes for Tree

--- lib/em-dir-watcher/tree.rb
+++ lib/em-dir-watcher/tree.rb
@@ -5,20 +5,25 @@ class Entry
   
   attr_reader :relative_path
   
-  def initialize full_path, relative_path
-    @full_path = full_path
+  def initialize tree, relative_path
+    @tree = tree
     @relative_path = relative_path
   end
-  
+
+  def full_path
+    if @relative_path.empty? then @tree.full_path else File.join(@tree.full_path, @relative_path) end
+  end
+
   def entries
-    (Dir.entries(@full_path) - ['.', '..']).collect do |entry|
+    (Dir.entries(full_path) - ['.', '..']).collect do |entry|
       entry_relative_path = if @relative_path.empty? then entry else File.join(@relative_path, entry) end
-      Entry.new File.join(@full_path, entry), entry_relative_path
-    end
+      next nil if @tree.excludes? entry_relative_path
+      Entry.new @tree, entry_relative_path
+    end.compact
   end
 
   def recursive_file_entries
-    if FileTest.directory? @full_path
+    if FileTest.directory? full_path
       entries.collect { |entry| entry.recursive_file_entries }.flatten
     else
       self
@@ -27,16 +32,65 @@ class Entry
 
 end
 
+class RegexpMatcher
+  def initialize re
+    @re = re
+  end
+  def matches? relative_path
+    relative_path =~ @re
+  end
+end
+
+class PathGlobMatcher
+  def initialize glob
+    @glob = glob
+  end
+  def matches? relative_path
+    File.fnmatch?(@glob, relative_path)
+  end
+end
+
+class NameGlobMatcher
+  def initialize glob
+    @glob = glob
+  end
+  def matches? relative_path
+    File.fnmatch?(@glob, File.basename(relative_path))
+  end
+end
+
 # Computes fine-grained (per-file) change events for a given directory tree,
 # using coarse-grained (per-subtree) events for optimization.
 class Tree
 
+  attr_reader :full_path
+  attr_reader :excludes
+
   def initialize full_path
-    @root_entry = Entry.new full_path, ''
+    @full_path = full_path
+    @root_entry = Entry.new self, ''
+    self.excludes = []
   end
   
+  def excludes= new_excludes
+    @excludes = new_excludes
+    @exclude_matchers = new_excludes.collect do |exclusion|
+      if Regexp === exclusion
+        RegexpMatcher.new exclusion
+      elsif exclusion.include? '/'
+        PathGlobMatcher.new exclusion
+      else
+        NameGlobMatcher.new exclusion
+      end
+    end
+  end
+
+  def excludes? relative_path
+    @exclude_matchers.any? { |matcher| matcher.matches? relative_path }
+  end
+
   def full_file_list
-    @root_entry.recursive_file_entries.collect { |entry| entry.relative_path }
+    @root_entry.recursive_file_entries.collect { |entry| entry.relative_path }.sort
   end
 
 end
--- test/test_tree.rb
+++ test/test_tree.rb
@@ -27,4 +27,65 @@ class TestTreeFileList < Test::Unit::TestCase
     assert_equal "bar/foo", @tree.full_file_list.join(", ").strip
   end
 
+  should "should return a sorted list of files" do
+    FileUtils.touch File.join(TEST_DIR, 'aa')
+    FileUtils.touch File.join(TEST_DIR, 'zz')
+    FileUtils.mkdir File.join(TEST_DIR, 'bar')
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'foo')
+    assert_equal "aa, bar/foo, zz", @tree.full_file_list.join(", ").strip
+  end
+
+end
+
+class TestTreeExcludes < Test::Unit::TestCase
+
+  def setup
+    FileUtils.rm_rf TEST_DIR
+    FileUtils.mkdir_p TEST_DIR
+
+    FileUtils.mkdir File.join(TEST_DIR, 'bar')
+    FileUtils.mkdir File.join(TEST_DIR, 'bar', 'boo')
+
+    FileUtils.touch File.join(TEST_DIR, 'aa')
+    FileUtils.touch File.join(TEST_DIR, 'biz')
+    FileUtils.touch File.join(TEST_DIR, 'zz')
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'foo')
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'biz')
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'biz.html')
+    FileUtils.touch File.join(TEST_DIR, 'bar', 'boo', 'bizzz')
+
+    @list = ['aa', 'biz', 'zz', 'bar/foo', 'bar/biz', 'bar/biz.html', 'bar/boo/bizzz'].sort
+
+    @tree = EMDirWatcher::Tree.new TEST_DIR
+  end
+
+  def join list
+    list.join(", ").strip
+  end
+
+  should "ignore a single file excluded by path" do
+    @tree.excludes = ['bar/biz']
+    assert_equal join(@list - ['bar/biz']), join(@tree.full_file_list)
+  end
+
+  should "ignore files excluded by name" do
+    @tree.excludes = ['biz']
+    assert_equal join(@list - ['biz', 'bar/biz']), join(@tree.full_file_list)
+  end
+
+  should "ignore files excluded by name glob" do
+    @tree.excludes = ['biz*']
+    assert_equal join(@list - ['biz', 'bar/biz', 'bar/biz.html', 'bar/boo/bizzz']), join(@tree.full_file_list)
+  end
+
+  should "ignore a directory excluded by name glob" do
+    @tree.excludes = ['bo*']
+    assert_equal join(@list - ['bar/boo/bizzz']), join(@tree.full_file_list)
+  end
+
+  should "ignore a files and directories excluded by regexp" do
+    @tree.excludes = [/b/]
+    assert_equal join(['aa', 'zz']), join(@tree.full_file_list)
+  end
+
 end
